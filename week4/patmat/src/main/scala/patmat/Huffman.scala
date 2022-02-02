2dcdbc799dcb4f37f305fc397b9a1546f767d6b3
bpratomo <bpratomo@myrsm.nl>
2022-02-02 14:30:54 +0000

    General commit

--- week4/patmat/src/main/scala/patmat/Huffman.scala
+++ week4/patmat/src/main/scala/patmat/Huffman.scala
@@ -140,7 +140,14 @@ trait Huffman extends HuffmanInterface:
    * If `trees` is a list of less than two elements, that list should be returned
    * unchanged.
    */
-  def combine(trees: List[CodeTree]): List[CodeTree] = ???
+  def combine(trees: List[CodeTree]): List[CodeTree] = 
+    trees match {
+      case first :: second :: Nil => trees
+      case first :: second :: tail => {
+        
+
+      }
+    }
 
   /**
    * This function will be called in the following way:
