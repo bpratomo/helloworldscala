6ed7efb69be046176b9e20967032c760e3a7a4ff
bpratomo <bpratomo@myrsm.nl>
2022-02-12 07:07:27 +0000

    finished the 4 checks and investigating the mismatch

--- 2_Functional Program Design/week1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ 2_Functional Program Design/week1/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -6,7 +6,13 @@ import Gen.*
 import Prop.forAll
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap:
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = oneOf(
+    const(empty),
+    for
+      x <- arbitrary[Int]
+      h <- genHeap
+    yield insert(x, h)
+  )
   given Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
@@ -14,3 +20,52 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap:
     findMin(insert(m, h)) == m
   }
 
+  property("two min in empty") = forAll { (h: H) =>
+    if isEmpty(h) then
+      val lvl1 = insert(1, h)
+      val lvl2 = insert(2, lvl1)
+      findMin(lvl2) == 1
+    else findMin(h) == findMin(h)
+  }
+
+  property("one del in empty should be empty") = forAll { (h: H) =>
+    if isEmpty(h) then
+      val inserted = insert(1, h)
+      val deleted = deleteMin(inserted)
+      deleted == empty
+    else empty == empty
+
+  }
+
+  property("minimum is sorted") = forAll { (h: H) =>
+    //println("====================================================")
+    def iter(h: H, acc: List[Int]): List[Int] =
+      //println(s"h is $h,acc is $acc")
+      if isEmpty(h) then acc
+      else
+        val min = findMin(h)
+        val newAcc = min :: acc
+        iter(deleteMin(h), newAcc)
+
+    val reversed = iter(h, Nil)
+    reversed
+      .sliding(2)
+      .forall(p =>
+        p match
+          case Nil        => true
+          case List(_)    => true
+          case List(x, y) => x >= y
+      )
+
+  }
+
+  property("melding should be ordered") = forAll { (h1raw: H, h2raw: H) =>
+    println("=======================================")
+    val h1 = if isEmpty(h1raw) then insert(Int.MinValue, empty) else h1raw
+    val h2 = if isEmpty(h2raw) then insert(Int.MinValue, empty) else h2raw
+    val h12 = meld(h1, h2)
+    println(s"h1 is $h1, min h1 is ${findMin(h1)}")
+    println(s"h2 is $h2, min h1 is ${findMin(h2)}")
+    println(s"h12 is $h12, min h1 is ${findMin(h12)}")
+    findMin(h12) == findMin(h1) || findMin(h12) == findMin(h2)
+  }
